b4edeb307fafd8b9873970c6ae6bd0c651e8a13f
VenpuDev <venpu.chile@gmail.com>
2025-10-01 01:48:12 -0300

    feat: Agrega el componente Footer a la landing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from 'react'
 import './globals.css'
-import { GoogleAnalytics } from '@/components/google-analytics' // <-- 1. IMPORTA EL COMPONENTE
+import { GoogleAnalytics } from '@/components/google-analytics'
+import { Analytics } from '@vercel/analytics/react'
+import { Footer } from '@/components/footer' // <-- 1. IMPORTA EL FOOTER
 
 export const metadata = {
   title: 'Venpu Plus Landing',
@@ -15,10 +17,12 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body>
-        <GoogleAnalytics /> {/* <-- 2. AÑADE EL COMPONENTE AQUÍ */}
+        <GoogleAnalytics />
         <Suspense fallback={<div>Cargando...</div>}>
           {children}
         </Suspense>
+        <Analytics />
+        <Footer /> {/* <-- 2. AÑADE EL FOOTER AQUÍ */}
       </body>
     </html>
   )
